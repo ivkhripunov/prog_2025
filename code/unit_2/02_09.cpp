53c127e72978a83dd904a2ecbb6f7bf5f9e73e4b
ivkhripunov <khripunov.iv@phystech.edu>
2025-11-02 14:05:54 +0300

    оставил только натуральные числа

--- code/unit_2/02_09.cpp
+++ code/unit_2/02_09.cpp
@@ -6,8 +6,6 @@
 #include <numeric>
 
 [[nodiscard]] int gcd_iterative(int a, int b) {
-    a = std::abs(a);
-    b = std::abs(b);
     while (b) {
         a %= b;
         std::swap(a, b);
@@ -16,23 +14,17 @@
 }
 
 [[nodiscard]] int gcd_recursive(int a, int b) {
-    a = std::abs(a);
-    b = std::abs(b);
     return b ? gcd_recursive(b, a % b) : a;
 }
 
 [[nodiscard]] int lcm(int a, int b) {
-    a = std::abs(a);
-    b = std::abs(b);
     return a / gcd_iterative(a, b) * b;
 }
 
 TEST(TASK_02_09, GCD_LCM) {
     const std::vector<std::pair<int, int>> test_pairs = {{6,   10},
-                                                         {-6,  10},
-                                                         {-6,  -10},
                                                          {24,  0},
-                                                         {-24, 0}};
+                                                         {53, 26}};
 
     for (const auto &pair: test_pairs) {
         const int gcd_ref = std::gcd(pair.first, pair.second);
