d7dc288334eea16d96107e9a52206ecc724ee872
ivkhripunov <khripunov.iv@phystech.edu>
2025-11-02 21:53:15 +0300

    сделал 03_10

--- code/unit_2/03_10.cpp
+++ code/unit_2/03_10.cpp
@@ -4,36 +4,32 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-#include <cassert>
 #include <cmath>
 #include <istream>
 #include <numeric>
 #include <ostream>
 #include <sstream>
 #include <vector>
+#include <gtest/gtest.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-class Rational
-{
+class Rational {
 public :
 
-    /* explicit */ Rational(int num = 0, int den = 1) : m_num(num), m_den(den)
-    {
+    /* explicit */ Rational(int num = 0, int den = 1) : m_num(num), m_den(den) {
         reduce();
     }
 
 //  -------------------------------------------------------------------------------------------
 
-    explicit operator double() const
-    {
+    explicit operator double() const {
         return 1.0 * m_num / m_den;
     }
 
 //  -------------------------------------------------------------------------------------------
 
-    auto & operator+=(Rational const & other)
-    {
+    auto &operator+=(Rational const &other) {
         auto lcm = std::lcm(m_den, other.m_den);
 
         m_num = m_num * (lcm / m_den) + other.m_num * (lcm / other.m_den);
@@ -47,15 +43,13 @@ public :
 
 //  -------------------------------------------------------------------------------------------
 
-    auto & operator-=(Rational const & other)
-    {
+    auto &operator-=(Rational const &other) {
         return *this += Rational(other.m_num * -1, other.m_den);
     }
 
 //  -------------------------------------------------------------------------------------------
 
-    auto & operator*=(Rational const & other)
-    {
+    auto &operator*=(Rational const &other) {
         m_num *= other.m_num;
 
         m_den *= other.m_den;
@@ -67,75 +61,72 @@ public :
 
 //  -------------------------------------------------------------------------------------------
 
-    auto & operator/=(Rational const & other)
-    {
+    auto &operator/=(Rational const &other) {
         return *this *= Rational(other.m_den, other.m_num);
     }
 
 //  -------------------------------------------------------------------------------------------
 
-    auto const operator++(int) { auto x = *this; *this += 1; return x; }
+    auto const operator++(int) {
+        auto x = *this;
+        *this += 1;
+        return x;
+    }
 
-    auto const operator--(int) { auto x = *this; *this -= 1; return x; }
-
-//  -------------------------------------------------------------------------------------------
-
-    auto & operator++() { *this += 1; return *this; }
-
-    auto & operator--() { *this -= 1; return *this; }
-
-//  -------------------------------------------------------------------------------------------
-
-    friend auto operator+ (Rational lhs, Rational const & rhs) { return lhs += rhs; }
-
-    friend auto operator- (Rational lhs, Rational const & rhs) { return lhs -= rhs; }
-
-    friend auto operator* (Rational lhs, Rational const & rhs) { return lhs *= rhs; }
-
-    friend auto operator/ (Rational lhs, Rational const & rhs) { return lhs /= rhs; }
-
-//  -------------------------------------------------------------------------------------------
-
-    friend auto operator< (Rational const & lhs, Rational const & rhs)
-    {
-        return lhs.m_num * rhs.m_den < rhs.m_num * lhs.m_den;
+    auto const operator--(int) {
+        auto x = *this;
+        *this -= 1;
+        return x;
     }
 
 //  -------------------------------------------------------------------------------------------
 
-    friend auto operator> (Rational const & lhs, Rational const & rhs) { return  (rhs < lhs); }
+    auto &operator++() {
+        *this += 1;
+        return *this;
+    }
 
-    friend auto operator<=(Rational const & lhs, Rational const & rhs) { return !(lhs > rhs); }
-
-    friend auto operator>=(Rational const & lhs, Rational const & rhs) { return !(lhs < rhs); }
-
-//  -------------------------------------------------------------------------------------------
-
-    friend auto operator==(Rational const & lhs, Rational const & rhs)
-    {
-        return !(lhs < rhs) && !(rhs < lhs);
+    auto &operator--() {
+        *this -= 1;
+        return *this;
     }
 
 //  -------------------------------------------------------------------------------------------
 
-    friend auto & operator>>(std::istream & stream, Rational & rational)
-    {
+    friend auto operator+(Rational lhs, Rational const &rhs) { return lhs += rhs; }
+
+    friend auto operator-(Rational lhs, Rational const &rhs) { return lhs -= rhs; }
+
+    friend auto operator*(Rational lhs, Rational const &rhs) { return lhs *= rhs; }
+
+    friend auto operator/(Rational lhs, Rational const &rhs) { return lhs /= rhs; }
+
+//  -------------------------------------------------------------------------------------------
+
+    friend auto operator<=>(Rational const &lhs, Rational const &rhs) {
+        return lhs.m_num * rhs.m_den <=> rhs.m_num * lhs.m_den;
+    }
+
+    friend auto operator==(Rational const &lhs, Rational const &rhs) {
+        return lhs.m_num * rhs.m_den == rhs.m_num * lhs.m_den;
+    }
+
+//  -------------------------------------------------------------------------------------------
+
+    friend auto &operator>>(std::istream &stream, Rational &rational) {
         return (stream >> rational.m_num).ignore() >> rational.m_den;
     }
 
 //  -------------------------------------------------------------------------------------------
 
-    friend auto & operator<<(std::ostream & stream, Rational const & rational)
-    {
+    friend auto &operator<<(std::ostream &stream, Rational const &rational) {
         return stream << rational.m_num << '/' << rational.m_den;
     }
 
 private :
 
-    void reduce()
-    {
-        if (m_den < 0)
-        {
+    void reduce() {
+        if (m_den < 0) {
             m_num = -m_num;
 
             m_den = -m_den;
@@ -155,52 +146,51 @@ private :
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-auto equal(double x, double y, double epsilon = 1e-6)
-{
+auto equal(double x, double y, double epsilon = 1e-6) {
     return std::abs(x - y) < epsilon;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-int main()
-{
+TEST(RATIONAL, ALL_TEST) {
+
     Rational x = 1, y(2, 1);
 
 //  -----------------------------------------------------------------------
 
 //	std::vector < int > vector_1 = 5; // error
 
-    std::vector < int > vector_2(5);
+    std::vector<int> vector_2(5);
 
-    std::vector < int > vector_3 = { 1, 2, 3, 4, 5 };
-
-//  -----------------------------------------------------------------------
-
-    assert(equal(static_cast < double > (x), 1));
+    std::vector<int> vector_3 = {1, 2, 3, 4, 5};
 
 //  -----------------------------------------------------------------------
 
-//	assert(x.operator+=(y) == Rational(3, 1)); // bad
+    ASSERT_TRUE(equal(static_cast < double > (x), 1));
 
 //  -----------------------------------------------------------------------
 
-    assert((x += y) == Rational(+3, 1));
-
-    assert((x -= y) == Rational(+1, 1));
-
-    assert((x *= y) == Rational(+2, 1));
-
-    assert((x /= y) == Rational(+1, 1));
+//	ASSERT_TRUE(x.operator+=(y) == Rational(3, 1)); // bad
 
 //  -----------------------------------------------------------------------
 
-    assert((x ++  ) == Rational(+1, 1)); // support : compiler-explorer.com
+    ASSERT_TRUE((x += y) == Rational(+3, 1));
 
-    assert((x --  ) == Rational(+2, 1));
+    ASSERT_TRUE((x -= y) == Rational(+1, 1));
 
-    assert((  ++ y) == Rational(+3, 1)); // support : compiler-explorer.com
+    ASSERT_TRUE((x *= y) == Rational(+2, 1));
 
-    assert((  -- y) == Rational(+2, 1));
+    ASSERT_TRUE((x /= y) == Rational(+1, 1));
+
+//  -----------------------------------------------------------------------
+
+    ASSERT_TRUE((x++) == Rational(+1, 1)); // support : compiler-explorer.com
+
+    ASSERT_TRUE((x--) == Rational(+2, 1));
+
+    ASSERT_TRUE((++y) == Rational(+3, 1)); // support : compiler-explorer.com
+
+    ASSERT_TRUE((--y) == Rational(+2, 1));
 
 //  -----------------------------------------------------------------------
 
@@ -216,43 +206,43 @@ int main()
 
 //  -----------------------------------------------------------------------
 
-//	assert(operator+(x, y) == Rational(3, 1)); // bad
+//	ASSERT_TRUE(operator+(x, y) == Rational(3, 1)); // bad
 
 //  -----------------------------------------------------------------------
 
-    assert((x +  y) == Rational(+3, 1));
+    ASSERT_TRUE((x + y) == Rational(+3, 1));
 
-    assert((x -  y) == Rational(-1, 1));
+    ASSERT_TRUE((x - y) == Rational(-1, 1));
 
-    assert((x *  y) == Rational(+2, 1));
+    ASSERT_TRUE((x * y) == Rational(+2, 1));
 
-    assert((x /  y) == Rational(+1, 2));
-
-//  -----------------------------------------------------------------------
-
-    assert((x += 1) == Rational(+2, 1));
-
-//	assert((1 += x) == Rational(+3, 1)); // error
-
-    assert((x +  1) == Rational(+3, 1));
-
-    assert((1 +  y) == Rational(+3, 1));
-
-    assert((1 +  1) == Rational(+2, 1));
+    ASSERT_TRUE((x / y) == Rational(+1, 2));
 
 //  -----------------------------------------------------------------------
 
-    assert((x <  y) == 0);
+    ASSERT_TRUE((x += 1) == Rational(+2, 1));
 
-    assert((x >  y) == 0);
+//	ASSERT_TRUE((1 += x) == Rational(+3, 1)); // error
 
-    assert((x <= y) == 1);
+    ASSERT_TRUE((x + 1) == Rational(+3, 1));
 
-    assert((x >= y) == 1);
+    ASSERT_TRUE((1 + y) == Rational(+3, 1));
 
-    assert((x == y) == 1);
+    ASSERT_TRUE((1 + 1) == Rational(+2, 1));
 
-    assert((x != y) == 0);
+//  -----------------------------------------------------------------------
+
+    ASSERT_TRUE((x < y) == 0);
+
+    ASSERT_TRUE((x > y) == 0);
+
+    ASSERT_TRUE((x <= y) == 1);
+
+    ASSERT_TRUE((x >= y) == 1);
+
+    ASSERT_TRUE((x == y) == 1);
+
+    ASSERT_TRUE((x != y) == 0);
 
 //  -----------------------------------------------------------------------
 
@@ -262,13 +252,15 @@ int main()
 
 //  -----------------------------------------------------------------------
 
-    stream_1 >> x;
+    stream_1 >>
+             x;
 
-    stream_2 << x;
+    stream_2 <<
+             x;
 
 //  -----------------------------------------------------------------------
 
-    assert(stream_2.str() == stream_1.str());
+    ASSERT_TRUE(stream_2.str() == stream_1.str());
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
