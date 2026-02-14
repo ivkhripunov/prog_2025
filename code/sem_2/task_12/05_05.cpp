869c78c4875a731a2c129680d9c85d2a44c4689a
ivan_khripunov <khripunov.iv@phystech.edu>
2026-02-14 17:36:29 +0300

    добавил 04_04

---
+++ code/sem_2/task_12/05_05.cpp
@@ -0,0 +1,268 @@
+//
+// Created by ivankhripunov on 02.11.2025.
+//
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <cmath>
+#include <istream>
+#include <numeric>
+#include <ostream>
+#include <sstream>
+#include <vector>
+#include <gtest/gtest.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+class Rational {
+public :
+
+    /* explicit */ Rational(T num = 0, T den = 1) : m_num(num), m_den(den) {
+        reduce();
+    }
+
+//  -------------------------------------------------------------------------------------------
+
+    explicit operator double() const {
+        return 1.0 * m_num / m_den;
+    }
+
+//  -------------------------------------------------------------------------------------------
+
+    auto &operator+=(Rational const &other) {
+        auto lcm = std::lcm(m_den, other.m_den);
+
+        m_num = m_num * (lcm / m_den) + other.m_num * (lcm / other.m_den);
+
+        m_den = lcm;
+
+        reduce();
+
+        return *this;
+    }
+
+//  -------------------------------------------------------------------------------------------
+
+    auto &operator-=(Rational const &other) {
+        return *this += Rational(other.m_num * -1, other.m_den);
+    }
+
+//  -------------------------------------------------------------------------------------------
+
+    auto &operator*=(Rational const &other) {
+        m_num *= other.m_num;
+
+        m_den *= other.m_den;
+
+        reduce();
+
+        return *this;
+    }
+
+//  -------------------------------------------------------------------------------------------
+
+    auto &operator/=(Rational const &other) {
+        return *this *= Rational(other.m_den, other.m_num);
+    }
+
+//  -------------------------------------------------------------------------------------------
+
+    auto const operator++(int) {
+        auto x = *this;
+        *this += 1;
+        return x;
+    }
+
+    auto const operator--(int) {
+        auto x = *this;
+        *this -= 1;
+        return x;
+    }
+
+//  -------------------------------------------------------------------------------------------
+
+    auto &operator++() {
+        *this += 1;
+        return *this;
+    }
+
+    auto &operator--() {
+        *this -= 1;
+        return *this;
+    }
+
+//  -------------------------------------------------------------------------------------------
+
+    friend auto operator+(Rational lhs, Rational const &rhs) { return lhs += rhs; }
+
+    friend auto operator-(Rational lhs, Rational const &rhs) { return lhs -= rhs; }
+
+    friend auto operator*(Rational lhs, Rational const &rhs) { return lhs *= rhs; }
+
+    friend auto operator/(Rational lhs, Rational const &rhs) { return lhs /= rhs; }
+
+//  -------------------------------------------------------------------------------------------
+
+    friend auto operator<=>(Rational const &lhs, Rational const &rhs) {
+        return lhs.m_num * rhs.m_den <=> rhs.m_num * lhs.m_den;
+    }
+
+    friend bool operator==(Rational const &lhs, Rational const &rhs) {
+        return lhs.m_num * rhs.m_den == rhs.m_num * lhs.m_den;
+    }
+
+//  -------------------------------------------------------------------------------------------
+
+    friend auto &operator>>(std::istream &stream, Rational &rational) {
+        return (stream >> rational.m_num).ignore() >> rational.m_den;
+    }
+
+//  -------------------------------------------------------------------------------------------
+
+    friend auto &operator<<(std::ostream &stream, Rational const &rational) {
+        return stream << rational.m_num << '/' << rational.m_den;
+    }
+
+private :
+
+    void reduce() {
+        if (m_den < 0) {
+            m_num = -m_num;
+
+            m_den = -m_den;
+        }
+
+        auto gcd = std::gcd(m_num, m_den);
+
+        m_num /= gcd;
+
+        m_den /= gcd;
+    }
+
+//  -------------------------------------------------------------------------------------------
+
+    T m_num = 0, m_den = 1;
+};
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+auto equal(double x, double y, double epsilon = 1e-6) {
+    return std::abs(x - y) < epsilon;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+TEST(RATIONAL, ALL_TEST) {
+
+    Rational x = 1, y(2, 1);
+
+//  -----------------------------------------------------------------------
+
+//	std::vector < int > vector_1 = 5; // error
+
+    std::vector<int> vector_2(5);
+
+    std::vector vector_3 = {1, 2, 3, 4, 5};
+
+//  -----------------------------------------------------------------------
+
+    ASSERT_TRUE(equal(static_cast < double > (x), 1));
+
+//  -----------------------------------------------------------------------
+
+//	ASSERT_TRUE(x.operator+=(y) == Rational(3, 1)); // bad
+
+//  -----------------------------------------------------------------------
+
+    ASSERT_TRUE((x += y) == Rational(+3, 1));
+
+    ASSERT_TRUE((x -= y) == Rational(+1, 1));
+
+    ASSERT_TRUE((x *= y) == Rational(+2, 1));
+
+    ASSERT_TRUE((x /= y) == Rational(+1, 1));
+
+//  -----------------------------------------------------------------------
+
+    ASSERT_TRUE((x++) == Rational(+1, 1)); // support : compiler-explorer.com
+
+    ASSERT_TRUE((x--) == Rational(+2, 1));
+
+    ASSERT_TRUE((++y) == Rational(+3, 1)); // support : compiler-explorer.com
+
+    ASSERT_TRUE((--y) == Rational(+2, 1));
+
+//  -----------------------------------------------------------------------
+
+//	x++++; // error
+
+//  -----------------------------------------------------------------------
+
+    [[maybe_unused]] auto z = 0;
+
+//  -----------------------------------------------------------------------
+
+//	z++++; // error
+
+//  -----------------------------------------------------------------------
+
+//	ASSERT_TRUE(operator+(x, y) == Rational(3, 1)); // bad
+
+//  -----------------------------------------------------------------------
+
+    ASSERT_TRUE((x + y) == Rational(+3, 1));
+
+    ASSERT_TRUE((x - y) == Rational(-1, 1));
+
+    ASSERT_TRUE((x * y) == Rational(+2, 1));
+
+    ASSERT_TRUE((x / y) == Rational(+1, 2));
+
+//  -----------------------------------------------------------------------
+
+    ASSERT_TRUE((x += 1) == Rational(+2, 1));
+
+//	ASSERT_TRUE((1 += x) == Rational(+3, 1)); // error
+
+    ASSERT_TRUE((x + 1) == Rational(+3, 1));
+
+    ASSERT_TRUE((1 + y) == Rational(+3, 1));
+
+    ASSERT_TRUE((1 + 1) == Rational(+2, 1));
+
+//  -----------------------------------------------------------------------
+
+    ASSERT_TRUE((x < y) == 0);
+
+    ASSERT_TRUE((x > y) == 0);
+
+    ASSERT_TRUE((x <= y) == 1);
+
+    ASSERT_TRUE((x >= y) == 1);
+
+    ASSERT_TRUE((x == y) == 1);
+
+    ASSERT_TRUE((x != y) == 0);
+
+//  -----------------------------------------------------------------------
+
+    std::stringstream stream_1("1/2");
+
+    std::stringstream stream_2;
+
+//  -----------------------------------------------------------------------
+
+    stream_1 >>
+             x;
+
+    stream_2 <<
+             x;
+
+//  -----------------------------------------------------------------------
+
+    ASSERT_TRUE(stream_2.str() == stream_1.str());
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
