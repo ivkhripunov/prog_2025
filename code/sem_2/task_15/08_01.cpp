33df1764163963f70415f94d383d4704fa9b859e
ivan_khripunov <khripunov.iv@phystech.edu>
2026-03-08 18:19:22 +0300

    сделал 08_01

---
+++ code/sem_2/task_15/08_01.cpp
@@ -0,0 +1,43 @@
+//
+// Created by ivan on 08.03.2026.
+//
+
+#include <gtest/gtest.h>
+#include <cstring>
+
+class Entity_v1 {
+    int v1_;
+
+public:
+    Entity_v1(const int v1) : v1_(v1) {
+    }
+
+    [[nodiscard]] int v1() const { return v1_; }
+};
+
+class Entity_v2 {
+public:
+    int v2;
+};
+
+// 1 способ
+TEST(task_08_01, reinterpret) {
+    Entity_v1 entity_v1(10);
+    ASSERT_EQ(entity_v1.v1(), 10);
+
+    Entity_v2& entity_v2 = reinterpret_cast<Entity_v2&>(entity_v1);
+    entity_v2.v2 = 100;
+
+    ASSERT_EQ(entity_v1.v1(), 100);
+}
+
+// 2 способ
+TEST(task_08_01, memory) {
+    Entity_v1 entity_v1(10);
+    ASSERT_EQ(entity_v1.v1(), 10);
+
+    const int new_v1 = 100;
+    std::memcpy(&entity_v1, &new_v1, sizeof(int));
+
+    ASSERT_EQ(entity_v1.v1(), 100);
+}
