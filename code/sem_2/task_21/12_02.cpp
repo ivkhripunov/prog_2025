4d6f7b061151865a01bcca7be9fa05daa1307cb4
ivan_khripunov <khripunov.iv@phystech.edu>
2026-05-18 00:59:35 +0300

    исправил решение

--- code/sem_2/task_21/12_02.cpp
+++ code/sem_2/task_21/12_02.cpp
@@ -1,6 +1,2 @@
-//
-// Created by ivan on 11.05.2026.
-//
-
 #include <cstdio>
-int main() {const char *s = "#include <cstdio>%cint main(){const char*s=%c%s%c;std::printf(s,10,34,s,34,10);}%c";std::printf(s, 10, 34, s, 34, 10);}
+int main(){const char*s="#include <cstdio>%cint main(){const char*s=%c%s%c;std::printf(s,10,34,s,34,10);}%c";std::printf(s,10,34,s,34,10);}
